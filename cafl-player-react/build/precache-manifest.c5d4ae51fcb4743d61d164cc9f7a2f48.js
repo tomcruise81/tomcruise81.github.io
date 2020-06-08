d916d796027f83a2091d735e9c315b43bf2741e7
Sean Dukehart <tomcruise81@users.noreply.github.com>
2020-06-08 16:31:15 -0400

    More explicit location

---
+++ cafl-player-react/build/precache-manifest.c5d4ae51fcb4743d61d164cc9f7a2f48.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b410e83f292147bfad9501fa41102d2c",
+    "url": "/cafl-player-react/build/index.html"
+  },
+  {
+    "revision": "975ca734d5664a843ab7",
+    "url": "/cafl-player-react/build/static/js/2.56fdfb9f.chunk.js"
+  },
+  {
+    "revision": "bb5eb84bc8effceedb71",
+    "url": "/cafl-player-react/build/static/js/main.d66c204a.chunk.js"
+  },
+  {
+    "revision": "cf2a9f5513bfd1c2ad2f",
+    "url": "/cafl-player-react/build/static/js/runtime-main.2f5ee007.js"
+  }
+]);
--- cafl-player-react/build/service-worker.js
+++ cafl-player-react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cafl-player-react/precache-manifest.15d5e7f5b20616e1be7731e6832c2ecc.js"
+  "/cafl-player-react/build/precache-manifest.c5d4ae51fcb4743d61d164cc9f7a2f48.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cafl-player-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cafl-player-react/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
