99d1ab63d5da655ac47e9ff98809a61322bc4b27
Sean Dukehart <tomcruise81@users.noreply.github.com>
2020-06-08 16:21:06 -0400

    Renamed repo

---
+++ cafl-player-react/build/precache-manifest.77aeb7cadd6eb32ed10b1dc1cedee64b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29f683c29a4b587fea99517ef605100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef0cbf56eecfa2393710",
+    "url": "/static/js/2.7245f70c.chunk.js"
+  },
+  {
+    "revision": "2bf6ceb03cf212f9d838",
+    "url": "/static/js/main.293daaa6.chunk.js"
+  },
+  {
+    "revision": "46235ffef8a55c1419da",
+    "url": "/static/js/runtime-main.9abee9fe.js"
+  }
+]);
