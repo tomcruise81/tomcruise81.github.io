8ad3907580048e11ea0c5226dd293f5ea0332179
Sean Dukehart <tomcruise81@users.noreply.github.com>
2020-06-09 13:41:44 -0400

    Scrapped Tone.js; Re-use some cafl-player3 code

--- cafl-player-react/src/ShuttleControls/index.js
+++ cafl-player-react/src/ShuttleControls/index.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import PlayIcon from '@material-ui/icons/PlayArrow';
 import PauseIcon from '@material-ui/icons/Pause';
-import Tone from "tone";
 
 const useStyles = makeStyles(theme => ({
     fab: {
@@ -13,48 +12,154 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-export default function ShuttleControls({presets, options}) {
+export default function ShuttleControls({ presets, options }) {
     const classes = useStyles();
     // const theme = useTheme();
     const [playing, setPlaying] = React.useState(false);
+    const oscillator = React.useRef(null);
+    const audioCtx = React.useRef(null);
+    // const polySynth = React.useRef(null);
+    // const sequence = React.useRef(null);
 
-    async function handlePlaying() {
-        await Tone.start();
-        const transport = Tone.Transport;
+    const secToMs = 1000;
+    let playClickedTimeMs = 0;
+    let analyser;
+    let bufferLength;
+    let timeDomainDataArray;
+    let currentFrequency;
+    let timeouts = [];
+    let totalTime;
 
-        if (playing) {
-            transport.stop();
-            //Clear out the timeline
-            transport.cancel();
-        } else {
-            //These may be indifferent
-            transport.timeSignature = [4, 4];
-            transport.bpm.value = 60;
-
-            const secondsPerFrequency = 3 * 60;
-            let timeOffset = 0;
-
-            for (let presetIndex = 0; presetIndex < presets.length; presetIndex++) {
-                let preset = presets[presetIndex];
-                if (preset.frequencies) {
-                    for (let frequencyIndex = 0; frequencyIndex < preset.frequencies.length; frequencyIndex++) {
-                        let frequency = preset.frequencies[frequencyIndex];
-                        new Tone.OmniOscillator(frequency, options.waveform)
-                            .toMaster()
-                            .sync()
-                            .start(timeOffset)
-                            .stop(timeOffset + secondsPerFrequency);
-                        timeOffset += secondsPerFrequency;
-                    }
-                }
+    function terminateAudio() {
+        if (oscillator.current) {
+            try {
+                oscillator.current.onended = function () { };
+                // oscillator.current.frequency.cancelScheduledValues(audioCtx.current.currentTime);
+                // console.log(`Audio Context State: ${audioCtx.current.state}`);
+                // console.log('Stopping oscillator');
+                oscillator.current.stop(0);
+            } catch (err) {
+                // console.error('Error cleaning up oscillator');
+                // console.error(err);
+                // console.error(err.stack);
             }
-
-            transport.setLoopPoints(0, timeOffset);
-            transport.loop = options.loop;
-
-            transport.start(0, 0);
         }
-        setPlaying(!playing && presets.length > 0);
+
+        oscillator.current = undefined;
+        analyser = undefined;
+        // WARNING: Don't terminate the audioCtx, since doing so prevents repeats and requires a new button click
+        // audioCtx.current = undefined;
+    }
+
+    function initializeAudio() {
+        terminateAudio();
+
+        // create web audio api context
+        audioCtx.current = audioCtx.current || new (window.AudioContext || window.webkitAudioContext)();
+        // audioCtx.current.onstatechange = function () {
+        //     console.log(`Audio Context State: ${audioCtx.current.state}`);
+        // };
+        analyser = audioCtx.current.createAnalyser();
+
+        // create Oscillator node
+        oscillator.current = audioCtx.current.createOscillator();
+
+        oscillator.current.connect(analyser);
+        analyser.connect(audioCtx.current.destination);
+    }
+
+    function indicateFrequency(frequency, secondsFromNow) {
+        timeouts.push(
+            setTimeout(() => {
+                currentFrequency = frequency;
+            }, secondsFromNow * secToMs)
+        );
+    }
+
+    function playClick() {
+        stopClick();
+
+        initializeAudio();
+
+        analyser.fftSize = 1024;
+
+        // drawClearCanvas();
+
+        oscillator.current.type = options.waveform;
+        const secondsValue = 60 * 3;//parseInt($('#seconds').val());
+        let startOfNextFrequencyViaAudioCtx = audioCtx.current.currentTime;
+        let startOfNextFrequencyFromNow = 0;
+
+        let frequencies = [];
+        for (let presetIndex = 0; presetIndex < presets.length; presetIndex++) {
+            let preset = presets[presetIndex];
+            if (preset.frequencies) {
+                frequencies = frequencies.concat(preset.frequencies)
+            }
+        }
+
+        for (let frequency of frequencies) {
+            oscillator.current.frequency.setValueAtTime(frequency, startOfNextFrequencyViaAudioCtx);
+            indicateFrequency(frequency, startOfNextFrequencyFromNow);
+            startOfNextFrequencyViaAudioCtx += secondsValue;
+            startOfNextFrequencyFromNow += secondsValue;
+        }
+
+        bufferLength = analyser.frequencyBinCount;
+        timeDomainDataArray = new Uint8Array(bufferLength);
+
+        oscillator.current.onended = function () {
+            console.log("onEnded event signled");
+            if (options.loop) {
+                setTimeout(() => {
+                    console.log("Repeating");
+                    playClick();
+                }, 0);
+            } else {
+                stopClick();
+            }
+        };
+
+        totalTime = startOfNextFrequencyFromNow * 1000;
+
+        oscillator.current.start();
+        oscillator.current.stop(startOfNextFrequencyViaAudioCtx);
+        // drawCanvas();
+    }
+
+    function stopClick() {
+        terminateAudio();
+
+        // drawClearCanvas();
+
+        while (timeouts.length) {
+            clearTimeout(timeouts.pop());
+        }
+        indicateFrequency(0, 0);
+        // $('#elapsed-value').val('');
+        // $('#remaining-value').val('');
+        totalTime = undefined;
+    }
+
+    function handlePlaying() {
+        const isPlay = !playing && presets.length > 0;
+        if (isPlay) {
+            playClickedTimeMs = new Date().getTime();
+            stopClick();
+            if (audioCtx.current) {
+              audioCtx.current.close();
+              audioCtx.current = undefined;
+            }
+            playClick();
+        }
+        else {
+            stopClick();
+            if (audioCtx.current) {
+                audioCtx.current.close();
+                audioCtx.current = undefined;
+            }
+        }
+        setPlaying(isPlay);
     }
 
     return (
